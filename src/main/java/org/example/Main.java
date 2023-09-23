bab7a05cbf1d1525a9b2734bad3664fb6dd8619c
vahed-pat <vahedpathan31@gmail.com>
2023-09-22 18:47:29 -0600

    push to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ import javax.mail.MessagingException;
 
 public class Main {
     public static void main(String[] args) throws MessagingException {
-        System.out.println(OtpReader.getOtp("a71983628@gmail.com", "fulm huoz xnsi ygnh", "Your secret login code"));
+        System.out.println(OtpReader.getOtp("email", "password", "Your secret login code"));
     }
 }
